9c712ef90413bf70cdd040643578c92db3b7504e
iDanteh <105389532+iDanteh@users.noreply.github.com>
2025-01-19 19:24:47 -0600

    Ajuste para usar RailWay

--- src/database/conexion.js
+++ src/database/conexion.js
@@ -1,9 +1,18 @@
 import { Sequelize } from 'sequelize';
+import dotenv from 'dotenv';
 
-const sequelize = new Sequelize('platforms', 'root', 'LeninRonaldo717', {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306
-});
+dotenv.config();
+
+const sequelize = new Sequelize(
+    process.env.DB_NAME || 'platforms',
+    process.env.DB_USER || 'root',
+    process.env.DB_PASSWORD || 'LeninRonaldo717',
+    {
+        host: process.env.DB_HOST || 'localhost',
+        dialect: 'mysql',
+        port: process.env.DB_PORT || 3306,
+        logging: false,
+    }
+);
 
 export default sequelize;
